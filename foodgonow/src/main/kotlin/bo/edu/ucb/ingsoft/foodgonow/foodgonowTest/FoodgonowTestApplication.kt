5041c948631dd2e8e3c0005568d159d40a101e0f
Ariel Monroy <ariel.monroy@ucb.edu.bo>
2022-03-02 17:41:26 -0400

    configuracion inicial

---
+++ foodgonow/src/main/kotlin/bo/edu/ucb/ingsoft/foodgonow/foodgonowTest/FoodgonowTestApplication.kt
@@ -0,0 +1,11 @@
+package bo.edu.ucb.ingsoft.foodgonow.foodgonowTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodgonowTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodgonowTestApplication>(*args)
+}
---
+++ foodgonow/src/test/kotlin/bo/edu/ucb/ingsoft/foodgonow/foodgonowTest/FoodgonowTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.foodgonow.foodgonowTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodgonowTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
