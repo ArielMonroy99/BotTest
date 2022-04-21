3065f26e1bccbb2a04a59bdff01e2bcef36347ce
Ariel Monroy <ariel.monroy@ucb.edu.bo>
2022-04-21 17:27:10 -0400

    Arreglos en la estructura del bot, cambios a los pedidos anteriores.

--- delivery/src/main/java/bo/edu/ucb/ingsoft/deliverybot/delivery/bl/PlateBl.java
+++ delivery/src/main/java/bo/edu/ucb/ingsoft/deliverybot/delivery/bl/PlateBl.java
@@ -1,5 +1,6 @@
 package bo.edu.ucb.ingsoft.deliverybot.delivery.bl;
 
+import bo.edu.ucb.ingsoft.deliverybot.delivery.dao.PlateDao;
 import bo.edu.ucb.ingsoft.deliverybot.delivery.dto.OrderDto;
 import bo.edu.ucb.ingsoft.deliverybot.delivery.dto.PlateDto;
 
@@ -7,17 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlateBl {
-
+    private PlateDao plateDao;
     public PlateBl(){
 
     }
     public List<PlateDto> TodayMenu(Long chatID){
-        List<PlateDto> plates = new ArrayList<>();
-        plates.add(new PlateDto(1,"Sopa lenteja", 500.0, "Es una sopa rica en muchas cosas"));
-        plates.add(new PlateDto(2,"Lasaña de pollo", 45.5,"Es un plato muy famoso italiano"));
-        plates.add(new PlateDto(3,"Fricase", 40.0, "Un plato muy bueno xd"));
-        plates.add(new PlateDto(4,"Pollo dorado",60.0, "un plato muy delicioso"));
-
-        return plates;
+        plateDao = new PlateDao();
+        return plateDao.TodayMenu(chatID);
     }
 }
