4228b019ba46407cdacc71d3cd49af9910174f18
Ignacio Carvajal <josignaciol89@gmail.com>
2022-04-30 16:30:11 -0400

    BotEncargado

---
+++ deliveryencargado/src/main/java/bo/adu/ucb/ingsoft/deliverybotencargado/deliveryencargado/DeliveryencargadoApplication.java
@@ -0,0 +1,13 @@
+package bo.adu.ucb.ingsoft.deliverybotencargado.deliveryencargado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryencargadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryencargadoApplication.class, args);
+	}
+
+}
---
+++ deliveryencargado/src/test/java/bo/adu/ucb/ingsoft/deliverybotencargado/deliveryencargado/DeliveryencargadoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.adu.ucb.ingsoft.deliverybotencargado.deliveryencargado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryencargadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
